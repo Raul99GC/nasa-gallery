7ef0b568e52c5442a7940032e5797e0028ff4b85
Raul99GC <raulc99g@gmail.com>
2022-11-15 23:42:56 -0600

    probando y componiendo los componentes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import styles from './styles/Header.module.css'
 import heroImage from './images/mars-rover-hero.jpg'
 import NavMenu from './NavMenu'
+import FilterForm from '../FilterForm/FilterForm'
 
 const Header = () => {
-  return <header className={styles.header} style={{ backgroundImage: `url(${heroImage})` }}>
-    <NavMenu />
-  </header>
+  return (
+    <header className={styles.header} style={{ backgroundImage: `url(${heroImage})` }}>
+      <div className={styles.container}>
+        <NavMenu />
+        <FilterForm />
+      </div>
+    </header>
+  )
 }
 
 export default Header
--- src/components/Header/NavMenu.jsx
+++ src/components/Header/NavMenu.jsx
@@ -4,12 +4,14 @@ import styles from './styles/NavMenu.module.css'
 import { useButton } from '../../hooks/'
 
 import logoNasa from './images/pngwing.com.png'
-// import ButtonMenu from './ButtonMenu'
 import { GiHamburgerMenu } from 'react-icons/gi'
 import Menu from './Menu'
+// import UseFetch from '../../hooks/UseFetch'
 
 const NavMenu = () => {
   const { statusButton, onchange } = useButton(false)
+  // const { data } = UseFetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=1&api_key=cwBn1QPdGz2gf2Bq0BbTsBmWjwbERoFWfkk7m1hj')
+  // console.dir(data?.photos[0])
 
   return (
     <nav className={styles.navMenu}>

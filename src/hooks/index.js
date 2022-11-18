de4dd379c60fa8acea3bf4c714e9ff275f5600c3
Raul99GC <raulc99g@gmail.com>
2022-11-18 14:03:34 -0600

    corrigiendo nombre de useFetchImages, añadiendo funcion getFetchFilter y haciendo uso de el

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,4 +1,4 @@
-export { default as UseFetchImages } from './useFetchImages'
+export { default as useFetchImages } from './useFetchImages'
 export { default as useButton } from './useButton'
 export { default as useFrom } from './useForm'
 export { default as useFetchFilters } from './useFetchFilters'
--- src/hooks/useFetchImages.js
+++ src/hooks/useFetchImages.js
@@ -1,21 +1,38 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
-const UseFetchImages = (url = '', key = '') => {
+const useFetchImages = (url = '', key = '', filters = {}) => {
   const [page, setPage] = useState(1)
+  const [filter, setFilter] = useState(filters)
   const [state, setState] = useState({
     data: [],
+    imagesFilters: [],
     isLoading: true,
     hasError: null
   })
 
-  const getFetch = async () => {
+  const getFetchDefault = async () => {
     const { data } = await axios.get(`${url}&api_key=${key}&page=${page}`)
     const { photos } = data
     setState({
+      ...state,
       data: [...state.data, ...photos],
-      isLoading: false,
-      hasError: null
+      isLoading: false
+    })
+  }
+
+  const getFetchFilter = async (fil = {}) => {
+    console.log(fil)
+    const rover = fil.rover.toLowerCase()
+    const date = fil.date ? `&earth_date=${fil.date}` : ""
+    const sol = fil.sol ? `&sol=${fil.sol}` : ""
+    const URL = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?api_key=${key}&page=${page}${date}${sol}`
+    const { data } = await axios.get(URL)
+    const { photos } = data
+    setState({
+      ...state,
+      isLoading: true,
+      imagesFilters: [...state.imagesFilters, ...photos]
     })
   }
 
@@ -23,16 +40,46 @@ const UseFetchImages = (url = '', key = '') => {
     setPage(prev => prev + 1)
   }
 
+  const newFilter = (fil = {}) => {
+    setFilter(fil)
+  }
+
   useEffect(() => {
-    getFetch()
+    getFetchDefault()
+  }, [])
+
+  useEffect(() => {
+    if (Object.keys(filter).length > 0) {
+      setState({
+        ...state,
+        isLoading: false
+      })
+      getFetchFilter(filter)
+    } else {
+      getFetchDefault()
+    }
   }, [page])
 
+  useEffect(() => {
+    if (Object.keys(filter).length > 0) {
+      setState({
+        data: [],
+        imagesFilters: [],
+        isLoading: true,
+        hasError: null
+      })
+      getFetchFilter(filter)
+    }
+  }, [filter])
+
   return {
-    data: state.data,
+    data: state.imagesFilters.length > 0 ? state.imagesFilters : state.data,
     isLoading: state.isLoading,
     hasError: state.hasError,
-    newPage
+    newPage,
+    newFilter
+
   }
 }
 
-export default UseFetchImages
+export default useFetchImages

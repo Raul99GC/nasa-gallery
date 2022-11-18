e0f456da0ca470400c22155f7a8641839b144118
Raul99GC <raulc99g@gmail.com>
2022-11-17 23:39:48 -0600

    agregando la url por props y arreglando el css

--- src/pages/Gallery/ImageCard.jsx
+++ src/pages/Gallery/ImageCard.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import styles from './styles/ImageCard.module.css'
 
 import { MdZoomOutMap } from 'react-icons/md'
+import PropTypes from 'prop-types'
 
-const ImageCard = () => {
+const ImageCard = ({ url = '' }) => {
   return (
     <li className={styles.item}>
       <div className={styles.boxImage}>
-        <img className={styles.img} src="https://e00-telva.uecdn.es/assets/multimedia/imagenes/2018/05/19/15267444150887.jpg" alt="" />
+        <img className={styles.img} src={url} alt="" />
       </div>
       <div className={styles.overlay}>
         <div className={styles.overlayInf}>
@@ -16,6 +17,10 @@ const ImageCard = () => {
       </div>
     </li>
   )
+}
+
+ImageCard.propTypes = {
+  url: PropTypes.string.isRequired
 }
 
 export default ImageCard

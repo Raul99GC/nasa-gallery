1dfe8d99437db853cb768909e6e8ca73abb89ead
Raul99GC <raulc99g@gmail.com>
2022-11-18 14:53:39 -0600

    arreglando error de variables no usadas

--- src/components/Header/NavMenu.jsx
+++ src/components/Header/NavMenu.jsx
@@ -9,7 +9,7 @@ import logoNasa from './images/pngwing.com.png'
 // import useFetch from '../../hooks/UseFetch'
 
 const NavMenu = () => {
-  const { statusButton, onchange } = useButton(false)
+  // const { statusButton, onchange } = useButton(false)
   // const { data } = UseFetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=1&api_key=cwBn1QPdGz2gf2Bq0BbTsBmWjwbERoFWfkk7m1hj')
   // console.dir(data?.photos[0])
 
